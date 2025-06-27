03375d20b547dfc714c1ba058fcf1c26354f4ea9
kostakarathana <kostakarathanasopoulos@gmail.com>
2025-06-27 12:34:01 +1000

    same as prev

--- Refined Solutions/solution.py
+++ Refined Solutions/solution.py
@@ -1,4 +1,23 @@
+from hangman_game import Hangman
+from collections import defaultdict
+import numpy as np
+import time
 
 
-class Solution:
+
+class HangmanSolver:
+    def __init__(self) -> None:
+        pass
+
+    def setup(self) -> None:
+        WORDS_BY_LENGTH: defaultdict[str,list[str]] = defaultdict(list[str])
+
+        with open('text_files/words.txt', 'r') as f:
+            WORD_LIST: np.ndarray = np.array([line.strip().lower() for line in f if line.strip()])
+        for i in range(1,46):
+            WORDS_BY_LENGTH[f"{i}"] = np.array([word for word in WORD_LIST if len(word) == i]) # type: ignore
+    
+    def solve(self) -> None:
+        
+
 
